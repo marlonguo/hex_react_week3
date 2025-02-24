b5dc2ce0d4ae13580270ba1edab42581595f173d
marlon <marlon.guo@outlook.com>
2025-02-25 00:11:33 +0800

    vite.config base

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   // 開發中、產品路徑
   // hex_react_template 改成專案名稱
-  // base: process.env.NODE_ENV === "production" ? "/hex_react_week3/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/hex_react_week3/" : "/",
   plugins: [react()],
 });
